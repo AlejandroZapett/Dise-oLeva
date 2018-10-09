539cbd67069c3681cb16b1029231251959bea5f2
Alejandro Zapett <alejandro.zapett@gmail.com>
2018-10-09 03:48:07 -0500

    realizando cambios en el programa de matlab, archivo word, excel y capturas de pantalla

--- Matlab/proyectoLeva.m
+++ Matlab/proyectoLeva.m
@@ -7,15 +7,8 @@ clf(figure(1));
 
 %parametros del sistema:
 
-h=10;
-
-th0 = 0;
-th1 = 120;
-th2 = 240;
-th3 = 360;
-
-B1=th1-th0;
-B3=th3-th2;
+h=3;
+B=120;
 
 %Diagrama de x
 subplot(3,1,1);
@@ -25,24 +18,35 @@ ylabel('h [mm]');
 grid on;
 hold on;
 %subida de: 0 a h, 0 a 120º,    s se describe por C5
-for th=th0:1:th1;
-    s = (h)*((th/B1)-(1/(2*pi))*sin(2*pi*th/B1));
+a=1;
+for th=0:1:120;
+    sx1(a) = (h)*((th/B)-(1/(2*pi))*sin(2*pi*th/B));
     x1 = th;
-    s1 = s;
+    s1 = sx1(a);
     plot(x1, s1, 'k*');
+    a=a+1;
 end
+Theta1 = 0:1:120;
 %reposo de: h a h, 120º a 240º, s es una linea recta
-x2 = th1+1:1:th2;
-s2 = zeros(th1,th2)+h;
+x2 = 121:1:240;
+s2 = zeros(1, 120)+h;
+sx2 = s2;
 plot(x2, s2, 'r*');
-%bajada de: h a 0, 140º a 360º, s se describe por C6
-for th=th2+1:1:th3;
-    s = (h)*(1-(th/B3)+(1/(2*pi))*sin(2*pi*th/B3));
+Theta2 = x2;
+%bajada de: h a 0, 240º a 360º, s se describe por C6
+a=1;
+for th=241:1:360;
+    sx3(a) = (h)*(1-(th/B)+(1/(2*pi))*sin(2*pi*th/B))+2*h;
     x3 = th;
-    s3 = s+2*h;
+    s3 = sx3(a);
     plot(x3, s3, 'k*');
+    a=a+1;
 end
+Theta3 = 241:1:360;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+X = [Theta1,Theta2,Theta3]'
+Y = [sx1,s2,sx3]'
 
 %Diagrama de v
 subplot(3,1,2);
@@ -52,19 +56,19 @@ ylabel('h [mm]');
 grid on;
 hold on;
 %subida de: 0 a h, 0 a 120º,    s se describe por C5
-for th=th0:1:th1;
-    s = (h/B1)*(1-cos(2*pi*th/B1));
+for th=0:1:120;
+    s = (h/B)*(1-cos(2*pi*th/B));
     x1 = th;
     s1 = s;
     plot(x1, s1, 'k*');
 end
 %reposo de: h a h, 120º a 240º, s es una linea recta
-x2 = th1+1:1:th2;
-s2 = zeros(th1,th2);
-plot(x2, s2, 'r*');
-%bajada de: h a 0, 140º a 360º, s se describe por C6
-for th=th2+1:1:th3;
-    s = -(h/B1)*(1-cos(2*pi*th/B1));
+x2 = 121:1:240;
+sx2 = zeros(1, 120);
+plot(x2, sx2, 'r*');
+%bajada de: h a 0, 240º a 360º, s se describe por C6
+for th=241:1:360;
+    s = -(h/B)*(1-cos(2*pi*th/B));
     x3 = th;
     s3 = s;
     plot(x3, s3, 'k*');
@@ -79,21 +83,23 @@ ylabel('h [mm]');
 grid on;
 hold on;
 %subida de: 0 a h, 0 a 120º,    s se describe por C5
-for th=th0:1:th1;
-    s = ((2*h*pi)/(B1*B1))*(sin(2*pi*th/B1));
+for th=0:1:120;
+    s = ((2*h*pi)/(B*B))*(sin(2*pi*th/B));
     x1 = th;
     s1 = s;
     plot(x1, s1, 'k*');
 end
 %reposo de: h a h, 120º a 240º, s es una linea recta
-x2 = th1+1:1:th2;
-s2 = zeros(th1,th2);
+x2 = 121:1:240;
+s2 = zeros(1,120);
 plot(x2, s2, 'r*');
 %bajada de: h a 0, 140º a 360º, s se describe por C6
-for th=th2+1:1:th3;
-     s = -((2*h*pi)/(B1*B1))*(sin(2*pi*th/B1));
+for th=241:1:360;
+     s = -((2*h*pi)/(B*B))*(sin(2*pi*th/B));
     x3 = th;
     s3 = s;
     plot(x3, s3, 'k*');
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
